87ce6f03320313a0adaacf877dede5a41bee4825
pardoman <pardo.medina@gmail.com>
2014-11-27 17:07:31 -0800

    Active tab logic encapsulated into 'observeTab' variable.  Passing onTabChanged as argument.  Initializing in injectChatOverlay.

--- content.js
+++ content.js
@@ -51,6 +51,52 @@ var observeDOM = (function(){
     }
 })();
 
+// Source: http://stackoverflow.com/questions/1060008/is-there-a-way-to-detect-if-a-browser-window-is-not-currently-active
+var observeTab = (function() {
+	var hidden = "hidden";
+
+	// Standards:
+	if (hidden in document)
+		document.addEventListener("visibilitychange", onchange);
+	else if ((hidden = "mozHidden") in document)
+		document.addEventListener("mozvisibilitychange", onchange);
+	else if ((hidden = "webkitHidden") in document)
+		document.addEventListener("webkitvisibilitychange", onchange);
+	else if ((hidden = "msHidden") in document)
+		document.addEventListener("msvisibilitychange", onchange);
+	// IE 9 and lower:
+	else if ("onfocusin" in document)
+		document.onfocusin = document.onfocusout = onchange;
+	// All others:
+	else {
+		window.onpageshow = window.onpagehide
+		= window.onfocus = window.onblur = onchange;
+	}
+	
+	var clientCallback;
+	function onchange (evt) {
+		if (!clientCallback) return;
+		var evtMap = {
+			focus:true, 
+			focusin:true, 
+			pageshow:true, 
+			blur:false, 
+			focusout:false, 
+			pagehide:false
+		};
+		evt = evt || window.event;
+		if (evt.type in evtMap) {
+		  clientCallback(evtMap[evt.type]);
+		} else {
+		  clientCallback(this[hidden] ? false : true); //lol
+		}
+	}
+	
+	return function (callback) {
+		clientCallback = callback;
+	}
+})();
+
 // ***********************************
 // ******** Event Hooks **************
 // ***********************************
@@ -184,6 +230,7 @@ function injectChatOverlay(msg, sender, sendResponse) {
 	
 	// Listen to new incoming chats
 	observeDOM(twitchChatLines, processNewChat);
+	observeTab(onTabChanged);
 	pushComment("TwitchTvChat plugin enabled!");
 	
 	// Our main loop
@@ -232,44 +279,3 @@ chrome.runtime.onMessage.addListener(function(msg, sender, sendResponse) {
 			break;
 	}
 });
-
-
-// Source: http://stackoverflow.com/questions/1060008/is-there-a-way-to-detect-if-a-browser-window-is-not-currently-active
-(function() {
-  var hidden = "hidden";
-
-  // Standards:
-  if (hidden in document)
-    document.addEventListener("visibilitychange", onchange);
-  else if ((hidden = "mozHidden") in document)
-    document.addEventListener("mozvisibilitychange", onchange);
-  else if ((hidden = "webkitHidden") in document)
-    document.addEventListener("webkitvisibilitychange", onchange);
-  else if ((hidden = "msHidden") in document)
-    document.addEventListener("msvisibilitychange", onchange);
-  // IE 9 and lower:
-  else if ("onfocusin" in document)
-    document.onfocusin = document.onfocusout = onchange;
-  // All others:
-  else
-    window.onpageshow = window.onpagehide
-    = window.onfocus = window.onblur = onchange;
-
-  function onchange (evt) {
-	var evtMap = {
-		focus:true, 
-		focusin:true, 
-		pageshow:true, 
-		blur:false, 
-		focusout:false, 
-		pagehide:false
-	};
-
-    evt = evt || window.event;
-    if (evt.type in evtMap) {
-      onTabChanged(evtMap[evt.type]);
-    } else {
-      onTabChanged(this[hidden] ? false : true); //lol
-    }
-  }
-})();
