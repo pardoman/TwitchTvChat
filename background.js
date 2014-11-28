8841c70c6b79df1ee8f3f70b16eeb53d5c63e38e
pardoman <pardo.medina@gmail.com>
2014-11-28 01:11:54 -0800

    Replacing broswer-action with page-action.

--- background.js
+++ background.js
@@ -1,12 +1,4 @@
-/* When the browser-action button is clicked... */
-chrome.browserAction.onClicked.addListener(function(tab) {
-    chrome.tabs.sendMessage(tab.id, {
-            command: "inject_chat_overlay",
-            tabData: tab
-        });
-});
-
-/* Used to detect when the tab changes its own url. */
+/** Used to detect when the tab changes its own url. */
 chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
     if (changeInfo && changeInfo.status == "complete") {
         chrome.tabs.sendMessage(tabId, {
@@ -14,4 +6,31 @@ chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
                 tabData: tab
             });
     }
+});
+
+/** Only show Twitch button when on twitch site. */
+chrome.runtime.onInstalled.addListener(function() {
+    // Replace all rules ...
+    chrome.declarativeContent.onPageChanged.removeRules(undefined, function() {
+        // With a new rule ...
+        chrome.declarativeContent.onPageChanged.addRules([
+	        {
+                // That fires when a page's URL contains 'twitch' ...
+                conditions: [
+                    new chrome.declarativeContent.PageStateMatcher({
+                    pageUrl: { urlContains: 'twitch.tv' }
+                })],
+                // And shows the extension's page action.
+                actions: [ new chrome.declarativeContent.ShowPageAction() ]
+            }
+        ]);
+    });
+});
+
+/** When the page-action button is clicked... */
+chrome.pageAction.onClicked.addListener(function(tab) {
+    chrome.tabs.sendMessage(tab.id, {
+        command: "inject_chat_overlay",
+        tabData: tab
+    });
 });
