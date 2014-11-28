fd56da345ec12f4c78f61a87bf0c43bb6c155a35
pardoman <pardo.medina@gmail.com>
2014-11-27 18:44:39 -0800

    Moved helper functions into helpers.js

--- content.js
+++ content.js
@@ -26,76 +26,7 @@ var twitchVideoPlayer = null;
 var twitchChatLines = null;
 var twitchLastChatId = "ember0";
 
-// ***********************************
-// ********* Aux functions ***********
-// ***********************************
-// Source: http://stackoverflow.com/questions/3219758/detect-changes-in-the-dom
-var observeDOM = (function(){
-    var MutationObserver = window.MutationObserver || window.WebKitMutationObserver,
-        eventListenerSupported = window.addEventListener;
 
-    return function(obj, callback){
-        if( MutationObserver ){
-            // define a new observer
-            var obs = new MutationObserver(function(mutations, observer){
-                if( mutations[0].addedNodes.length || mutations[0].removedNodes.length )
-                    callback();
-            });
-            // have the observer observe foo for changes in children
-            obs.observe( obj, { childList:true, subtree:true });
-        }
-        else if( eventListenerSupported ){
-            obj.addEventListener('DOMNodeInserted', callback, false);
-            // obj.addEventListener('DOMNodeRemoved', callback, false);
-        }
-    }
-})();
-
-// Source: http://stackoverflow.com/questions/1060008/is-there-a-way-to-detect-if-a-browser-window-is-not-currently-active
-var observeTab = (function() {
-    var hidden = "hidden";
-
-    // Standards:
-    if (hidden in document)
-        document.addEventListener("visibilitychange", onchange);
-    else if ((hidden = "mozHidden") in document)
-        document.addEventListener("mozvisibilitychange", onchange);
-    else if ((hidden = "webkitHidden") in document)
-        document.addEventListener("webkitvisibilitychange", onchange);
-    else if ((hidden = "msHidden") in document)
-        document.addEventListener("msvisibilitychange", onchange);
-    // IE 9 and lower:
-    else if ("onfocusin" in document)
-        document.onfocusin = document.onfocusout = onchange;
-    // All others:
-    else {
-        window.onpageshow = window.onpagehide
-        = window.onfocus = window.onblur = onchange;
-    }
-    
-    var clientCallback;
-    function onchange (evt) {
-        if (!clientCallback) return;
-        var evtMap = {
-            focus:true, 
-            focusin:true, 
-            pageshow:true, 
-            blur:false, 
-            focusout:false, 
-            pagehide:false
-        };
-        evt = evt || window.event;
-        if (evt.type in evtMap) {
-          clientCallback(evtMap[evt.type]);
-        } else {
-          clientCallback(this[hidden] ? false : true); //lol
-        }
-    }
-    
-    return function (callback) {
-        clientCallback = callback;
-    }
-})();
 
 // ***********************************
 // ******** Event Hooks **************
@@ -242,8 +173,8 @@ function injectChatOverlay(msg, sender, sendResponse) {
     myContext2d = myCanvas.getContext("2d"); // TODO: Can this fail? check for null?
     
     // Listen to new incoming chats
-    observeDOM(twitchChatLines, processNewChatMessages);
-    observeTab(onTabChanged);
+    observeDOM(twitchChatLines, processNewChatMessages);    // helpers.js
+    observeTab(onTabChanged);                               // helpers.js
     processNewChatMessages(); // We find the id of the last chat message already present,
     myChatsToRender = [];     // and then we just flush the list.
     myNextTextIndex = 1;
---
+++ helpers.js
@@ -0,0 +1,70 @@
+// ***********************************
+// ********* Aux functions ***********
+// ***********************************
+// Source: http://stackoverflow.com/questions/3219758/detect-changes-in-the-dom
+var observeDOM = (function(){
+    var MutationObserver = window.MutationObserver || window.WebKitMutationObserver,
+        eventListenerSupported = window.addEventListener;
+
+    return function(obj, callback){
+        if( MutationObserver ){
+            // define a new observer
+            var obs = new MutationObserver(function(mutations, observer){
+                if( mutations[0].addedNodes.length || mutations[0].removedNodes.length )
+                    callback();
+            });
+            // have the observer observe foo for changes in children
+            obs.observe( obj, { childList:true, subtree:true });
+        }
+        else if( eventListenerSupported ){
+            obj.addEventListener('DOMNodeInserted', callback, false);
+            // obj.addEventListener('DOMNodeRemoved', callback, false);
+        }
+    }
+})();
+
+// Source: http://stackoverflow.com/questions/1060008/is-there-a-way-to-detect-if-a-browser-window-is-not-currently-active
+var observeTab = (function() {
+    var hidden = "hidden";
+
+    // Standards:
+    if (hidden in document)
+        document.addEventListener("visibilitychange", onchange);
+    else if ((hidden = "mozHidden") in document)
+        document.addEventListener("mozvisibilitychange", onchange);
+    else if ((hidden = "webkitHidden") in document)
+        document.addEventListener("webkitvisibilitychange", onchange);
+    else if ((hidden = "msHidden") in document)
+        document.addEventListener("msvisibilitychange", onchange);
+    // IE 9 and lower:
+    else if ("onfocusin" in document)
+        document.onfocusin = document.onfocusout = onchange;
+    // All others:
+    else {
+        window.onpageshow = window.onpagehide
+            = window.onfocus = window.onblur = onchange;
+    }
+
+    var clientCallback;
+    function onchange (evt) {
+        if (!clientCallback) return;
+        var evtMap = {
+            focus:true,
+            focusin:true,
+            pageshow:true,
+            blur:false,
+            focusout:false,
+            pagehide:false
+        };
+        evt = evt || window.event;
+        if (evt.type in evtMap) {
+            clientCallback(evtMap[evt.type]);
+        } else {
+            clientCallback(this[hidden] ? false : true); //lol
+        }
+    }
+
+    return function (callback) {
+        clientCallback = callback;
+    }
+})();
